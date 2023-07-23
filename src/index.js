a0fbcea48af61d25e49124a5617f5deed18a6abe
Linet <muthiigathoni@gmail.com>
2023-07-23 23:22:55 +0300

    Completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import ColoradoStateParks from "./ColoradoStateParks";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ColoradoStateParks from './ColoradoStateParks'; 
+import { trees, wildlife } from './parks/RockyMountain';
+import howManyParks from './parks/howManyParks'; 
+import MesaVerde from './parks/MesaVerde'; 
 
-ReactDOM.render(<ColoradoStateParks />, document.getElementById("root"));
+function App() {
+  howManyParks();
+
+  console.log(trees);
+  wildlife();
+
+  return (
+    <div>
+      <ColoradoStateParks />
+      <MesaVerde />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- src/parks/RockyMountain.js
+++ src/parks/RockyMountain.js
@@ -8,4 +8,15 @@ function elevation() {
   console.log("9583 ft");
 }
 
-export { trees, wildlife };
+function RockyMountain() {
+  return (
+    <div>
+      <h2>Rocky Mountain National Park</h2>
+      <p>Trees: {trees}</p>
+      <p>Wildlife: {wildlife()}</p>
+      <p>Elevation: {elevation()}</p>
+    </div>
+  );
+}
+
+export default RockyMountain;
